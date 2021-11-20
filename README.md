a2a2ac12ebd267fe670a54a7d48a3971db2a6ebb
deaned01 <deaned01@gmail.com>
2021-11-20 14:21:12 +1100

    commit for ms

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # SubDivHabitatModels
-R code to predict shared species (zeta diversity) and total diversity using zeta components as presented in the paper "A null model for quantifying the geometric effect of habitat subdivision on species diversity", published in Global Ecology and Biogeography, 2021.
+R code to predict shared species (zeta diversity) and total diversity using zeta components as presented in the paper A null model for quantifying the geometric effect of habitat subdivision on species diversity (Global Ecology and Biogeography, 2021).
 
-Includes test data (Barro Colorado Island 2005 census) and functions necessary to:
+Includes functions necessary to use the models for prediction of total species diversity, pairwise (Sorensen) dissimilarity, or the number of species confined to a single patch. This application requires only a species-abundance distribution and scaling relationship for the c-parameter. 
 
-1. sample from a stem-mapped forest at a given sampling grain 
-2. estimate the c-parameter for alpha diversity at that grain
-3. predict shared species for m samples
-4. calculate gamma diversity and single-patch endemics for the m samples
+Alternatively, one can compare model predictions against observed data. This requires data recording the identity and location of every individual in a continuous landscape. The researcher samples this landscape at a given quadrat size, repeating this process many times to calculate an expectation. 
+
+The non-random models (Eq 5/6 in the text) can then be fit to the observed alpha diversity for each grain using the (known) species abundance distribution and sampling grain to estimate fitting parameter c. Predictions can then be compared between predicted and observed values.
+
+See 'Worked examples' html for a demonstration. 
---
+++ functions.R
@@ -0,0 +1,263 @@
+# R functions from 'A null model for quantifying the geometric effect of habitat subdivision on species diversity'
+# 
+
+#***************************************************************************************************************
+# Shared spp (zeta) functions ####
+# One can use either the negative binomial (quick, recommended) or 
+# finite negative binomial (strictly speaking the correct model,for a finite area, but slow and requiring package 
+# to deal with the large combinatorial calcs - numerical differences are trivial)
+
+# 1. negative binomial 
+fitSS.NB <- function(area, sad, cpar, m, tota = 500000){
+  # to fit modelled SS with known cpar (par) and SAD
+  
+  # Arguments:
+  #### area = area at which alpha estimated in same units as tota (usually sampling grain in m2)
+  #### sad = species abundance distribution (global)
+  #### par = c-par for 'area' sampling grain 
+  #### m = desired number of shared samples
+  #### tota = global area (default assumes 50-ha fdp plot)
+  
+  # Returns:
+  #### vector of zeta diversity of length m 
+  
+  pr.area = area/tota
+  
+  S0 <- length(sad)
+  kvec <- (sad*pr.area)/cpar
+  
+  pr.spp <- numeric()
+  for(i in 1:S0){
+    pr.spp[i] <-  (1 - (1 - pr.area)*(1 + sad[i]*pr.area/kvec[i])^-kvec[i])
+  }
+  
+  ssp <- numeric()
+  for(j in 1:m){
+    ssp[j] <- sum(pr.spp^j)} 
+  
+  return(ssp)
+} 
+
+# 2. Finite negative binomial
+# note this can be slow and the improvement in accuracy is unlikely to be worth the wait...
+# note too requires package Rmpfr to deal with the large numbers in the gamma function... 
+fitSS.FNB <- function(sad, cpar, area, tota=500000, m){
+  require(Rmpfr)
+  # sad = vector of abundances (ie SAD over study extent) 
+  # area =  subarea of interest
+  # tota = total area
+  # cpar = c parameter for that grain fit to mean alpha 
+  # m = number of samples of interest for shared and endemic count
+  
+  # Returns:
+  # vector of shared species in m samples
+  
+  pr.area = area/tota
+  
+  k <- (pr.area*sad)/cpar
+  
+  pr.spp <- c()
+  for(i in 1:length(sad)){
+    pr.spp[i] <- 1 - as.numeric((gamma(as(sad[i]+ k[i]/pr.area - k[i],"mpfr"))*
+                                   gamma(as(k[i]/pr.area,"mpfr")))/
+                                  (gamma(as(sad[i]+k[i]/pr.area,"mpfr"))*gamma(as(k[i]/pr.area - k[i],"mpfr"))))
+  }
+  
+  ssp <- c()
+  for(j in 1:m){
+    ssp[j] <- sum(pr.spp^j)
+  }
+  
+  return(ssp)
+}
+
+
+
+# 3. random placement
+
+fitss.RP <- function(area, tota = 500000, m, sad){
+  # a is area of interest
+  # A is total study extent for SAD (default value is for 50 ha in metres)
+  # returns vector of species shared in 1:m samples
+  mvec <- 1:m
+  rpmod <- c()
+  pr.area <- area/tota
+  
+  for(i in 1:length(mvec)){
+    rpmod[i] <- sum((1-(1- pr.area)^sad)^mvec[i])}
+  
+  return(rpmod)}
+
+
+#***************************************************************************************************************
+# Diversity functions ####
+# Once you have shared species for the relevant setting, the diversity calculations are all done the 
+# same way. Three functions are presented here (as used in the paper). 
+
+# All of these functions take one argument: zeta diversity (the mean number of shared spp) in m patches.
+# So, the mean number of species in m patches (alpha diversity), the mean number in 2... and so on up to m. 
+
+# See: 
+# Hui & McGeoch, Zeta diversity as a concept and metric that unifies incidence-based biodiversity patterns American Naturalist 2014 Vol. 184 Issue 5 Pages 684-694
+
+# gamma diversity
+gam.fn <- function(x){
+  # x is list of shared spp in m patches
+  sse <- x
+  len <- length(sse)
+  xp <- 1:len
+  nx <- (-1)^(xp+1) * sse * choose(len, xp)
+  gam <- sum(nx)
+  return(gam)
+}
+
+# beta diversity
+# x is list of shared spp in m patches)
+# fn returns expected Sorensen pairwise dissimilarity 
+bd.fn <- function(x){ 1-x[2]/x[1] } # 
+
+# SPE
+# to predict the number of species found in only one patch
+spe.fn <- function(x){
+  # x is a list of shared spp for m patches
+  # fn returns number of spp in only one patch
+  sser <- x
+  lenr <- length(sser)
+  xpr <- 1:lenr
+  # to calculate S(m-1)
+  xpr.1 <- 1:(lenr-1)
+  sser.1 <- sser[-max(xpr)]
+  nxr <- (-1)^(xpr+1) * sser * choose(lenr, xpr)
+  nxr.1 <- (-1)^(xpr.1+1) * sser.1 * choose((lenr-1), xpr.1)
+  gm <- sum(nxr)
+  gm.1 <- sum(nxr.1)
+  spe <- choose(lenr, 1)*(gm-gm.1)
+  return(spe)}
+
+
+#***************************************************************************************************************
+# Validation functions ####
+# To test model predictions against actual data, e.g., from a stem-mapped forest
+# 1. negative binomial
+
+fit.c.NB <- function(obs, area, sad, tota= 500000, low=0, upp=100){
+  # function to fit the 1-parameter fnb shared species model to a list of observed data
+  # basically a wrapper for cparls (see below)
+  
+  # Args:
+  # obs: alpha diversity at grain 'area'
+  # area: sampling grain in same units as tota
+  # sad: species abundance distribution
+  # tota: extent over which sad is calculated
+  # low, upp = limits over which to search for c - you will need to adjust to allow negative values 
+  # to fit to regular distributions - I find this can be a bit prickly, so several different values might be 
+  # necessary to get a fit.
+  
+  #est <- numeric() # fit1 is a logical flag to indicate convergence 
+    fit1 = tryCatch({
+      optim(fn = cdif.NB, par =  1, obs = obs, area = area, sad = sad, 
+            lower = low, upper = upp, method = "Brent")
+    }, warning = function(w) {
+      w
+    }, error = function(e) {
+      e  })
+    
+    if(!is(fit1,"error") & length(fit1$par[1])>0) {
+      fit <- 1
+      est <- fit1$par[1]
+    } else {
+      fit <- 0
+      est <- NA}
+    
+  out <- data.frame(cpar= est, fit=fit)
+  return(out)
+}
+
+cdif.NB <- function(area, sad, par, obs, tota = 500000){
+  # helper function to be used with numerical optimizer (eg optim() ) to find the value of 
+  # community scaling parameter c that minimises the absolute difference with observed alpha diversity
+  
+  # Arguments:
+  #### area = area at which alpha estimated in same units as tota
+  #### sad = species abundance distribution (global)
+  #### pars = c-par to fit, 
+  #### obs = observed shared species accumulated across the sites represented in the area vector
+  #### tota = global area (default assumes 50-ha fdp plot)
+  
+  # Returns: difference between observed and predicted value for passed c value
+  
+  pr.area = area/tota
+  cpar = as.numeric(par[1])
+  
+  S0 <- length(sad)
+  
+  calc <- numeric()
+  
+  kvec <- sad*pr.area/cpar
+  for(i in 1:S0){ 
+    calc[i] <-  (1 - (1-pr.area)*(1 + cpar)^-kvec[i])}
+  
+  srsim <- sum(calc) 
+  dif <- (srsim - obs)
+  
+  return(abs(dif))
+} 
+
+# 2. finite NB ####
+fit.c.fnb <- function(obs, area, sad, tota= 500000, low=-100, upp=100){
+  # function to fit the 1-parameter fnb shared species model to a list of observed data
+  # basically a wrapper for css.fnb (see below)
+  
+  # Args:
+  # obs: list of observed data. Can be either number of species at a grain or number shared
+  # 
+
+  # fit <- est <- numeric() # fit10r is a logical flag to indicate convergence; c20 is estimated cpar at that scale 
+ 
+  fit1 = tryCatch({
+      optim(fn = cdif.fnb, obs = obs, par = c(cpar = 1),  area = area, tota = tota, 
+            sad = sad, method="Brent", lower = low, upper = upp)
+    }, warning = function(w) {
+      w
+    }, error = function(e) {
+      e  })
+    
+    if(!is(fit1,"error") & length(fit1$par[1])>0) {
+      fit <- 1
+      est <- fit1$par[1]
+    } else {
+      fit <- 0
+      est <- NA}
+  out <- data.frame(cpar= est, fit=fit)
+  return(out)
+}
+
+cdif.fnb <- function(obs, pars, area, tota = 500000, sad){
+  # helper function to be used with numerical optimizer (eg optim() ) to find the value of 
+  # community scaling parameter c that minimises the absolute difference with observed alpha diversity
+
+  # Arguments:
+  # a = area, tota = total area, sad = species abundance dist.; pars = cpar value for optimizer; obs= observed alpha div
+  
+  require("Rmpfr")
+  cpar = pars[1]
+  
+  pr.area <- area/tota
+
+  prob.sar <- c()
+  
+  for(i in 1:length(sad)){
+    
+    prob.sar[i] <- 1 - as.numeric((gamma(as(sad[i]*(1+ 1/cpar - pr.area/cpar),"mpfr"))*
+                                   gamma(as(sad[i]/cpar,"mpfr")))/
+                                  (gamma(as(sad[i]*(1+ 1/cpar),"mpfr"))*
+                                     gamma(as(sad[i]/cpar*(1- pr.area),"mpfr"))))
+    }
+  
+  ssp <- sum(prob.sar)
+  
+  dif <- sum(abs(obs - ssp))
+  
+  return(dif)
+}
+
